e02a53741f9090d66d2e5fed5301f32ff4f21aa0
jmfgoliveira <joao.miguel.oliveira@ist.utl.pt>
2017-12-05 14:52:04 +0000

    fixed register requests and removed temp

--- Server/initial/src/main/java/server/Server.java
+++ Server/initial/src/main/java/server/Server.java
@@ -8,19 +8,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Server {
 
-    private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/register")
-    public String register(@RequestParam("email") String email,
+    public String register(@RequestParam("name") String name, @RequestParam("email") String email,
             @RequestParam("password") String password) {
+    	System.out.println("REGISTER");
+    	System.out.println("name: " + name);
     	System.out.println("email: " + email);
     	System.out.println("password: " + password);
-        return email + password;
+    	new User(counter.intValue(), name, password, email);
+        return name + email + password;
     }
+    
     @RequestMapping("/login")
-    public Register login(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Register(counter.incrementAndGet(),
-                            String.format(template, name));
+    public String login(@RequestParam("email") String email,
+            @RequestParam("password") String password) {
+    	System.out.println("LOGIN");
+    	System.out.println("email: " + email);
+    	System.out.println("password: " + password);
+    	if(email.equals("ola@ola.com") && password.equals("ola"))
+    		return "LOGGED IN";
+        return new String("TODO");
+    }
+    
+    @RequestMapping("/product")
+    public String product(@RequestParam("email") String email,
+            @RequestParam("password") String password) {
+    	System.out.println("LOGIN");
+    	System.out.println("email: " + email);
+    	System.out.println("password: " + password);
+    	if(email.equals("ola@ola.com") && password.equals("ola"))
+    		return "LOGGED IN";
+        return new String("TODO");
     }
 }
